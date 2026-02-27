f71096199fe3293bcfc6cc8f0e0819f41998cc03
shrutika-gawande <shrutikagawande80@gmail.com>
2026-02-27 13:48:17 +0530

    Designed form layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,40 @@ function App() {
     return (
         <form className='form'>
             <h1 className='mb-5'>Hotel Booking Form</h1>
-            <div className="row mb-3">
-                <div className="col">
-                    <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+            <div className='border p-4 m-4 w-60'>
+                <div className='mb-4'>
+                    <label className="">Check-in Date</label>
+                    <input type='date' className="form-control" />
                 </div>
-                <div className="col">
-                    <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
-                </div>
-            </div>
-            <div className="mb-4">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="example@gmail.com" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            {/* <div className="">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="*****" />
-            </div> */}
 
-            <div class="row g-3 mb-4">
-                <div class="col-sm-7">
-                    <input type="text" class="form-control" placeholder="City" aria-label="City"/>
+                <div className='mb-4'>
+                    <label className="">Check-out Date</label>
+                    <input type='date' className="form-control" />
                 </div>
-                <div class="col-sm">
-                    <input type="text" class="form-control" placeholder="State" aria-label="State"/>
+
+                <div className='mb-4'>
+                    <label>Room Type</label>
+                    <select className="form-select" aria-label="Default select example">
+                        <option value="1">Single</option>
+                        <option value="2">Double</option>
+                        <option value="3">Suite</option>
+                    </select>
                 </div>
-                <div class="col-sm">
-                    <input type="text" class="form-control" placeholder="Zip" aria-label="Zip"/>
+
+                <div className='mb-4'>
+                    <label>Guests</label>
+                    <input type='number' min="1" className="form-control" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Special Requests</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Any additional preferences..' rows="3"></textarea>
+                </div>
+
+                <div class="d-grid gap-2">
+                    <button type="submit" className="btn btn-primary">Confirm Booking</button>
                 </div>
             </div>
-
-            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     )
 }
