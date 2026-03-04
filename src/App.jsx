c89d85002824a17a5882b149d9d05b7152e64b15
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-04 12:10:05 +0530

    add Bootstrap validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
     const [checkIn, setCheckIn] = useState("");
     const [checkOut, setCheckOut] = useState("");
     const [error, setError] = useState("");
+    const [submit, setSubmit] = useState("");
+    const [wasValidated, setWasValidated] = useState(false);
 
     const today = new Date().toLocaleDateString("en-CA");
 
@@ -19,9 +21,36 @@ function App() {
         }
     };
 
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        if (!e.target.checkValidity()) {
+            setWasValidated(true);
+            return;
+        }
+
+        setWasValidated(true);
+        setSubmit("Booking Confirmed!");
+
+        setCheckIn("");
+        setCheckOut("");
+        setWasValidated(false);
+        e.target.reset();
+    }
+
     return (
 
-        <form className='form'>
+        <form
+            className={`form ${wasValidated ? 'was-validated' : ''}`}
+            noValidate
+            onSubmit={handleSubmitForm}
+        >
+            {submit && (
+                <div className="alert alert-success alert-dismissible fade show" role="alert">
+                    {submit}
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            )}
             {error && (
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     {error}
@@ -40,7 +69,9 @@ function App() {
                         value={checkIn}
                         onChange={(e) => setCheckIn(e.target.value)}
                         className="form-control"
+                        required
                     />
+                    <div className="invalid-feedback">Please select a check-in date.</div>
                 </div>
 
                 <div className='mb-4'>
@@ -51,30 +82,42 @@ function App() {
                         value={checkOut}
                         disabled={!checkIn}
                         onChange={handleCheckOutChange}
-                        className="form-control" />
+                        className="form-control"
+                        required
+                    />
+                    <div className="invalid-feedback">Please select a check-out date.</div>
                 </div>
 
-                <div className='mb-4'>
-                    <label>Room Type</label>
-                    <select className="form-select" aria-label="Default select example">
+                <div className='mb-4 has-validation'>
+                    <label for="roomType">Room Type</label>
+                    <select className="form-select" aria-label="Default select example" id="roomType" required>
                         <option value="1">Single</option>
                         <option value="2">Double</option>
                         <option value="3">Suite</option>
                     </select>
+                    <div className="invalid-feedback">
+                        Please choose a username.
+                    </div>
                 </div>
 
-                <div className='mb-4'>
+                <div class="row mb-4">
                     <label>Guests</label>
-                    <input type='number' min="1" className="form-control" />
+                    <div className="col">
+                        <input type="number" class="form-control" placeholder="adults" required min="1" />
+                        <div className="invalid-feedback">Please enter number of adults.</div>
+                    </div>
+                    <div className="col">
+                        <input type="number" class="form-control" placeholder="children" required min="0" />
+                    </div>
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Special Requests</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Any additional preferences..' rows="3"></textarea>
+                    <label htmlFor="textarea" className="form-label">Special Requests</label>
+                    <textarea className="form-control" id="textarea" placeholder='Any additional preferences..' rows="3"></textarea>
                 </div>
 
-                <div class="d-grid gap-2">
-                    <button type="submit" className="btn btn-primary">Confirm Booking</button>
+                <div className="d-grid gap-2">
+                    <button type="submit" className="btn btn-primary"> Confirm Booking </button>
                 </div>
             </div>
         </form>
