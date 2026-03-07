60dc7322dfa4f35ddd5415ca17dc6d584ff86d0c
shrutika-gawande <shrutikagawande80@gmail.com>
2026-03-07 19:03:35 +0530

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -40,100 +40,111 @@ function App() {
 
     return (
         <>
-        <img src='/image.png' alt='hotel image' className='background-img'/>
-        <form
-            className={`form ${wasValidated ? 'was-validated' : ''}`}
-            noValidate
-            onSubmit={handleSubmitForm}
-        >
-            {submit && (
-                <div className="alert alert-success alert-dismissible fade show" role="alert">
-                    {submit}
-                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            )}
-            {error && (
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    {error}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            )}
-            
-            <h1 className=''>Hotel Booking</h1>
-            <p className='mb-5'>Find and book your perfect stay with ease</p>
-
-            <div className='card-box w-60'>
-                <div class="row g-3 mb-4">
-                    <div class="col">
-                        <label className="">Full Name</label>
-                        <input type="text" class="form-control" placeholder="Enter the name" aria-label="First name" required />
+            <img src='/image.png' alt='hotel image' className='background-img' />
+            <form
+                className={`form ${wasValidated ? 'was-validated' : ''}`}
+                noValidate
+                onSubmit={handleSubmitForm}
+            >
+                {submit && (
+                    <div className="alert alert-success alert-dismissible fade show alert-fixed" role="alert">
+                        {submit}
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={() => setSubmit("")}
+                        ></button>
                     </div>
-                    <div class="col">
-                        <label className="">E-mail</label>
-                        <input type="email" class="form-control" placeholder="example@gmail.com" />
+                )}
+
+                {error && (
+                    <div className="alert alert-danger alert-dismissible fade show alert-fixed" role="alert">
+                        {error}
+                        <button
+                            type="button"
+                            className="btn-close"
+                            onClick={() => setError("")}
+                        ></button>
+                    </div>
+                )}
+
+                <h1 className=''>Hotel Booking</h1>
+                <p className='mb-5'>Find and book your perfect stay with ease</p>
+
+                <div className='card-box w-60'>
+                    <div className="row g-3 mb-4">
+                        <div className="col">
+                            <label htmlFor='fullName'>Full Name</label>
+                            <input type="text" id='fullName' className="form-control" placeholder="Enter the name" required />
+                        </div>
+                        <div className="col">
+                            <label htmlFor='email'>E-mail</label>
+                            <input type="email" id='email' className="form-control" placeholder="example@gmail.com" />
+                        </div>
+                    </div>
+
+                    <div className='mb-4'>
+                        <label htmlFor='checkIn'>Check-in Date</label>
+                        <input
+                            type='date'
+                            id='checkIn'
+                            min={today}
+                            value={checkIn}
+                            onChange={(e) => setCheckIn(e.target.value)}
+                            className="form-control"
+                            required
+                        />
+                        <div className="invalid-feedback">Please select a check-in date.</div>
+                    </div>
+
+                    <div className='mb-4'>
+                        <label htmlFor='checkOut'>Check-out Date</label>
+                        <input
+                            type='date'
+                            id='checkOut'
+                            min={checkIn}
+                            value={checkOut}
+                            disabled={!checkIn}
+                            onChange={handleCheckOutChange}
+                            className="form-control"
+                            required
+                        />
+                        <div className="invalid-feedback">Please select a check-out date.</div>
+                    </div>
+
+                    <div className='mb-4 has-validation'>
+                        <label htmlFor="roomType">Room Type</label>
+                        <select className="form-select" aria-label="Default select example" id="roomType" required>
+                            <option value="1">Single</option>
+                            <option value="2">Double</option>
+                            <option value="3">Suite</option>
+                        </select>
+                        <div className="invalid-feedback">
+                            Please choose a username.
+                        </div>
+                    </div>
+
+                    <div className="row mb-4">
+                        <label htmlFor='guests'>Guests</label>
+                        <div className="col">
+                            <input type="number" id='guests' className="form-control" placeholder="adults" required min="1" />
+                            <div className="invalid-feedback">Please enter number of adults.</div>
+                        </div>
+                        <div className="col">
+                            <input type="number" className="form-control" placeholder="children" required min="0" />
+                        </div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="request" className="form-label">Special Requests</label>
+                        <textarea className="form-control" id="request" placeholder='Any additional preferences..' rows="3"></textarea>
+                    </div>
+
+                    <div className="d-grid gap-2">
+                        <button type="submit" className="btn btn-primary"> Confirm Booking </button>
                     </div>
                 </div>
-                
-                <div className='mb-4'>
-                    <label className="">Check-in Date</label>
-                    <input
-                        type='date'
-                        min={today}
-                        value={checkIn}
-                        onChange={(e) => setCheckIn(e.target.value)}
-                        className="form-control"
-                        required
-                    />
-                    <div className="invalid-feedback">Please select a check-in date.</div>
-                </div>
-
-                <div className='mb-4'>
-                    <label className="">Check-out Date</label>
-                    <input
-                        type='date'
-                        min={checkIn}
-                        value={checkOut}
-                        disabled={!checkIn}
-                        onChange={handleCheckOutChange}
-                        className="form-control"
-                        required
-                    />
-                    <div className="invalid-feedback">Please select a check-out date.</div>
-                </div>
-
-                <div className='mb-4 has-validation'>
-                    <label for="roomType">Room Type</label>
-                    <select className="form-select" aria-label="Default select example" id="roomType" required>
-                        <option value="1">Single</option>
-                        <option value="2">Double</option>
-                        <option value="3">Suite</option>
-                    </select>
-                    <div className="invalid-feedback">
-                        Please choose a username.
-                    </div>
-                </div>
-
-                <div class="row mb-4">
-                    <label>Guests</label>
-                    <div className="col">
-                        <input type="number" class="form-control" placeholder="adults" required min="1" />
-                        <div className="invalid-feedback">Please enter number of adults.</div>
-                    </div>
-                    <div className="col">
-                        <input type="number" class="form-control" placeholder="children" required min="0" />
-                    </div>
-                </div>
-
-                <div className="mb-3">
-                    <label htmlFor="textarea" className="form-label">Special Requests</label>
-                    <textarea className="form-control" id="textarea" placeholder='Any additional preferences..' rows="3"></textarea>
-                </div>
-
-                <div className="d-grid gap-2">
-                    <button type="submit" className="btn btn-primary"> Confirm Booking </button>
-                </div>
-            </div>
-        </form>
+            </form>
         </>
     )
 }
